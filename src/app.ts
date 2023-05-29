f2aa4bb1c0b99995803c850773bf9610cb8e0fee
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-05-29 15:42:12 -0400

    refactor and move to article.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,93 +1,15 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import * as mongoose from 'mongoose';
-import { BodyRequest, ParamsRequest } from './types/RequestResponse';
+import articleRouter from './article';
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  }),
-);
-
 app.use(express.static('public'));
-
-const url = 'mongodb://127.0.0.1:27017/wikiDB';
-
-mongoose.connect(url).then(() => {
-  console.log('Connected to MongoDB');
-});
-
-type ArticleType = {
-  title: string;
-  content: string;
-};
-
-const articleSchema = new mongoose.Schema<ArticleType>({
-  title: {
-    type: String,
-    required: [true, 'Please check your data entry, no name specified!'],
-  },
-  content: {
-    type: String,
-    required: [true, 'Please check your data entry, no name specified!'],
-  },
-});
-
-const Article = mongoose.model('Article', articleSchema);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.get('/articles', (req, res) => {
-  Article.find()
-    .then((articles) => {
-      res.send(articles);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-});
-
-app.delete('/articles', (req: BodyRequest<ArticleType>, res) => {
-  Article.deleteMany()
-    .then(() => {
-      res.send('Successfully deleted all articles');
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-});
-
-app.delete('/articles/:id', (req: ParamsRequest<{ id: string }>, res) => {
-  const { id } = req.params;
-  console.log(id);
-  Article.findByIdAndRemove(id)
-    .then(() => {
-      res.send('Successfully deleted article');
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-});
-
-app.post('/articles', (req: BodyRequest<ArticleType>, res) => {
-  const { content, title } = req.body;
-  const article = new Article({
-    title,
-    content,
-  });
-  article
-    .save()
-    .then((article) => {
-      res.send(article);
-    })
-    .catch((err) => {
-      res.send(err);
-    });
-});
+app.use('/articles', articleRouter);
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
---
+++ src/article.ts
@@ -0,0 +1,87 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import * as mongoose from 'mongoose';
+
+const url = 'mongodb://127.0.0.1:27017/wikiDB';
+mongoose.connect(url).then(() => {
+  console.log('Connected to MongoDB');
+});
+
+import { BodyRequest, ParamsRequest } from './types/RequestResponse';
+
+type ArticleType = {
+  title: string;
+  content: string;
+};
+
+const articleSchema = new mongoose.Schema<ArticleType>({
+  title: {
+    type: String,
+    required: [true, 'Please check your data entry, no name specified!'],
+  },
+  content: {
+    type: String,
+    required: [true, 'Please check your data entry, no name specified!'],
+  },
+});
+
+const Article = mongoose.model('Article', articleSchema);
+
+const router = express.Router();
+
+router.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
+
+router
+  .route('/')
+  .get((req, res) => {
+    Article.find()
+      .then((articles) => {
+        res.send(articles);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  })
+  .post((req: BodyRequest<ArticleType>, res) => {
+    const { content, title } = req.body;
+    const article = new Article({
+      title,
+      content,
+    });
+    article
+      .save()
+      .then((article) => {
+        res.send(article);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  })
+  .delete((req: BodyRequest<ArticleType>, res) => {
+    Article.deleteMany()
+      .then(() => {
+        res.send('Successfully deleted all articles');
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  });
+
+router.delete('/:id', (req: ParamsRequest<{ id: string }>, res) => {
+  const { id } = req.params;
+  console.log(id);
+  Article.findByIdAndRemove(id)
+    .then(() => {
+      res.send('Successfully deleted article');
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+export default router;
