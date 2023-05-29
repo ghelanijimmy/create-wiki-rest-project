8615c30bfd0e24f8c78f959d000853a928ece985
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-05-29 14:50:25 -0400

    fix nodemon and add starting app.ts setup with db setup

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import ejs from 'ejs';
-import mongoose from 'mongoose';
+import * as mongoose from 'mongoose';
 
 const app = express();
-
+app.set('view engine', 'ejs');
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -12,6 +11,42 @@ app.use(
 );
 
 app.use(express.static('public'));
+
+const url = 'mongodb://127.0.0.1:27017/wikiDB';
+
+mongoose.connect(url).then(() => {
+  console.log('Connected to MongoDB');
+});
+
+const articleSchema = new mongoose.Schema<{
+  title: string;
+  content: string;
+}>({
+  title: {
+    type: String,
+    required: [true, 'Please check your data entry, no name specified!'],
+  },
+  content: {
+    type: String,
+    required: [true, 'Please check your data entry, no name specified!'],
+  },
+});
+
+const Article = mongoose.model('Article', articleSchema);
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/articles', (req, res) => {
+  Article.find()
+    .then((articles) => {
+      res.send(articles);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
